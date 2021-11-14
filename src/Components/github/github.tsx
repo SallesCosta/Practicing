394b8ade7517f725ea72d8e87fc7d7fe6c75aaeb
SallesCosta <newcapital.in@gmail.com>
2021-11-14 20:30:29 +0100

    repos gitHub

--- src/Components/github/github.tsx
+++ src/Components/github/github.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { Text, Input, Image, VStack, Box, Flex, FormControl, Button } from '@chakra-ui/react'
 
 export const Github = () => {
@@ -9,7 +9,8 @@ export const Github = () => {
   const [repos, setRepos] = useState('')
   const [avatar, setAvatar] = useState('')
   const [userInput, setUserInput] = useState('')
-  // const [error, setError] = useState(null)
+  const [error, setError] = useState(null)
+  const inputRef = useRef()
 
   // useEffect(() => {
   //   // fetch('https://api.github.com/users/exemple')
@@ -26,38 +27,48 @@ export const Github = () => {
     login,
     followers,
     following,
-    public_repos,
-    avatar_url,
-  }: any) => {
-    setName(name),
-      setUsername(login),
-      setFollowers(followers),
-      setFollowing(following),
-      setRepos(public_repos),
-      setAvatar(avatar_url)
-  }
+    // eslint-disable-next-line camelcase
+    public_repos, avatar_url,
+  }: any) => { setName(name), setUsername(login), setFollowers(followers), setFollowing(following), setRepos(public_repos), setAvatar(avatar_url) }
 
   const handleSearch = (e: any) => {
     setUserInput(e.target.value)
+  }
+
+  const handleConfirm = (e: any) => {
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if (keyCode === ENTER) {
+      handleSubmit()
+    }
   }
 
   const handleSubmit = () => {
     fetch(`https://api.github.com/users/${userInput}`)
       .then(res => res.json())
       .then(data => {
-        setData(data)
+        if (data.message) {
+          setError(data.message)
+        } else {
+          setData(data)
+          setError(null)
+        }
       })
+    // inputRef=''
   }
+
   return (
     <Flex>
       <VStack>
         <FormControl>
-          <Input placeholder='Basic usage' onChange={handleSearch} />
+          <Input placeholder='Basic usage' onChange={handleSearch} onKeyUp={handleConfirm} />
           <Button onClick={handleSubmit} colorScheme='blue' variant='outline'>Submit</Button>
         </FormControl>
         <Box>
           <Image src={avatar} />
           <Text>usuário escolhido {name}</Text>
+          <Text>usuário escolhido {userName}</Text>
           <Text>Follower {followers}</Text>
           <Text>Following {following}</Text>
           <Text>Repositórios {repos}</Text>
